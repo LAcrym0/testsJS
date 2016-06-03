3f490457fda283bb06b78d45250f30878133e7a7
Ollivier <lacrym095@gmail.com>
2016-06-03 15:02:33 +0200

    Added GET /contacts

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ api.get('/', function(req, res, next){
   res.send('Hello world');
 });
 
+api.get('/contacts', function(req, res, next){
+  res.send([]);
+});
+
 console.log('API listening on port 3000');
 api.listen(3000);
 
